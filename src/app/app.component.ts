fe7ffa979333c301f83963f7b346e8e8325ce4f9
Jimboriffic <james.sliwa@gmail.com>
2026-03-16 11:31:26 +0100

    First changes added to app.components.ts and app.components.html
    image changes when a vehicle type is selected
    
    when you click on scooter, the subtype option disappears.
    
    time spent: 1 hour (excluding the setup of vscode + extensions and PATH configuration on Linux Mint)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
 
+type VehicleType = 'auto' | 'motor' | 'scooter';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  vehicleType: VehicleType = 'auto';
+  vehicleSubtype = 'Hatchback';
+  licensePlate = '';
+
+  // Available subtypes options rely on selected vehicle type
+   subtypes = {
+    auto: ['Hatchback', 'Sedan', 'Station', 'Cabriolet', 'Coupé', 'MPV', 'Terreinauto'],
+    motor: ['All-road', 'Naked', 'Enduro', 'Race', 'Toermotor', 'Chopper', 'Zijspan'],
+    scooter: []
+  };
+  
+// For current vehicle selection it matches the image path
+  get vehicleImage(): string {
+    return `assets/${this.vehicleType}.jpg`;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
@@ -10,7 +11,8 @@ import { StoreModule } from '@ngrx/store';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    FormsModule // Necessary for form bindings like ngModel
   ],
   providers: [],
   bootstrap: [AppComponent]
